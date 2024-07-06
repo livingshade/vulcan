985d643f1d39ead233e35eda2b4aae2056993062
banruo <banruol2@illinois.edu>
2024-07-06 13:34:45 +0000

    Update

--- video_analytics/sampler.py
+++ video_analytics/sampler.py
@@ -1,6 +1,6 @@
 import os
 import random
-
+from torch.utils.data import Dataset
 UNIT_LEN = 1000
 
 class Sampler():
@@ -15,10 +15,12 @@ class Sampler():
     def update_weight():
         pass
     
-class VOiCERandomSampler(Sampler):
-    def __init__(self, keys):
-        self.keys = keys.copy()
+class NuImagesRandomSampler(Sampler):
+    def __init__(self, dataset: Dataset):
         self.random_idx = 0
+        self.ds = dataset
+        self.n = len(dataset)
+        self.keys = list(range(self.n))
         
     def init(self):
         self.random_idx = 0
@@ -34,4 +36,48 @@ class VOiCERandomSampler(Sampler):
         if method == "random":
             return self.random_sample()
         else:
-            raise Exception(f"Invalid sample method [{method}] for VOiCERandomSampler")
+            raise Exception(f"Invalid sample method [{method}]")
+        
+class NuImagesStratifiedSampler(Sampler):
+    def __init__(self, dataset, cluster):
+        self.ds = dataset
+        self.keys = list(cluster.keys()).copy()
+        self.k2g = cluster.copy()
+        self.n_groups = len(set(list(cluster.values())))
+        self.next_group = 0
+        self.group = [
+            [k for k in self.keys if self.k2g[k] == i] for i in range(self.n_groups)
+        ]
+        self.group_idx = [0] * self.n_groups
+        res = ""
+        for i in self.group:
+            res = res + " " + str(len(i))
+        print(f"Weighted Sampler: {res}")
+        
+    def init(self):        
+        self.next_group = 0
+        self.group_idx = [0] * self.n_groups
+        for i in range(self.n_groups):
+            random.shuffle(self.group[i])
+            
+        random.shuffle(self.keys)
+       
+    def sample_group(self):
+        key = random.choice(self.keys)
+        return self.k2g[key]
+    
+    def stratified_sample(self):
+        g = self.sample_group()
+        
+        idx = self.group_idx[g]
+        self.group_idx[g] = (self.group_idx[g] + 1) % len(self.group[g])
+        key = self.group[g][idx]
+        
+        return key
+    
+    def sample(self, method):
+        if method == "stratified":
+            return self.stratified_sample()
+        else:
+            raise Exception(f"Invalid sample method [{method}]")
+           
